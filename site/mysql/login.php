d73cbca0ffcbe8bf8e35d5d31525ed1d71f46368
monge95 <pedrokamonge@gmail.com>
2024-11-28 14:16:58 -0300

    não sei oq eu fiz

--- site/mysql/login.php
+++ site/mysql/login.php
@@ -1,40 +1,58 @@
-<?php
-    session_start();
-    // print_r($_REQUEST);
-    if(isset($_POST['submit']) && !empty($_POST['email']) && !empty($_POST['senha']))
-    {
-        // Acessa
-        include_once('config.php');
-        $email = $_POST['email'];
-        $senha = $_POST['senha'];
-
-        // print_r('Email: ' . $email);
-        // print_r('<br>');
-        // print_r('Senha: ' . $senha);
-
-        $sql = "SELECT * FROM usuarios WHERE email = '$email' and senha = '$senha'";
-
-        $result = $conexao->query($sql);
-
-        // print_r($sql);
-        // print_r($result);
-
-        if(mysqli_num_rows($result) < 1)
-        {
-            unset($_SESSION['email']);
-            unset($_SESSION['senha']);
-            header('Location: login.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela de login</title>
+    <style>
+        body{
+            font-family: Arial, Helvetica, sans-serif;
+            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
         }
-        else
-        {
-            $_SESSION['email'] = $email;
-            $_SESSION['senha'] = $senha;
-            header('Location: sistema.php');
+        div{
+            background-color: rgba(0, 0, 0, 0.6);
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%,-50%);
+            padding: 80px;
+            border-radius: 15px;
+            color: #fff;
         }
-    }
-    else
-    {
-        // Não acessa
-        header('Location: login.php');
-    }
-?>
+        input{
+            padding: 15px;
+            border: none;
+            outline: none;
+            font-size: 15px;
+        }
+        .inputSubmit{
+            background-color: dodgerblue;
+            border: none;
+            padding: 15px;
+            width: 100%;
+            border-radius: 10px;
+            color: white;
+            font-size: 15px;
+            
+        }
+        .inputSubmit:hover{
+            background-color: deepskyblue;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <a href="home.php">Voltar</a>
+    <div>
+        <h1>Login</h1>
+        <form action="testLogin.php" method="POST">
+            <input type="text" name="email" placeholder="Email">
+            <br><br>
+            <input type="password" name="senha" placeholder="Senha">
+            <br><br>
+            <input class="inputSubmit" type="submit" name="submit" value="Enviar">
+        </form>
+    </div>
+</body>
+</html>
